34645d0ae382c203ca12cf787a6915e40442d36e
Zane Pocock <zanepocock@gmail.com>
2021-10-30 11:27:57 -0500

    Basic data provider for Popcorn API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import * as React from "react";
-import { Admin } from 'react-admin';
-import jsonServerProvider from 'ra-data-json-server';
+import * as React from "react"
+import { Admin, Resource, ListGuesser } from 'react-admin'
+// import jsonServerProvider from 'ra-data-json-server'
+import dataProvider from './dataProvider'
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
-const App = () => <Admin dataProvider={dataProvider} />;
+// const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com')
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="accounts" list={ListGuesser} />
+    <Resource name="accounttypes" list={ListGuesser} />
+    <Resource name="blocks" list={ListGuesser} />
+    <Resource name="categories" list={ListGuesser} />
+    <Resource name="transactions" list={ListGuesser} />
+    <Resource name="transactiontypes" list={ListGuesser} />
+  </Admin>
+)
 
-export default App;
+export default App
