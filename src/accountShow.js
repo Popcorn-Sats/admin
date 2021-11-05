44f54ddae243f22781671a75a2d215eb249aa40b
Zane Pocock <zanepocock@gmail.com>
2021-11-05 17:01:52 -0500

    Add transactions button to account view

--- src/accountShow.js
+++ src/accountShow.js
@@ -1,9 +1,33 @@
-import * as React from "react";
-import { Show, SimpleShowLayout, TextField, DateField, RichTextField, NumberField, BooleanField } from 'react-admin';
+import * as React from "react"
+import { Show, SimpleShowLayout, TextField, DateField, RichTextField, NumberField, BooleanField } from 'react-admin'
+import { Button, } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import ReceiptIcon from '@material-ui/icons/Receipt'
 
-export const AccountShow = (props) => (
+const useStyles = makeStyles(theme => ({
+  root: {
+      padding: 20,
+      marginTop: theme.spacing(2),
+      marginBottom: '1em',
+  },
+  actions: {
+      [theme.breakpoints.down('md')]: {
+          padding: 0,
+          flexWrap: 'wrap',
+          '& a': {
+              marginTop: '1em',
+              marginLeft: '0!important',
+              marginRight: '1em',
+          },
+      },
+  },
+}));
+
+export const AccountShow = (props) => {
+  const classes = useStyles()
+  return (
     <Show {...props}>
-        <SimpleShowLayout>
+        <SimpleShowLayout className={classes.root}>
           <TextField source="id" />
           <TextField source="name" />
           <RichTextField source="notes" />
@@ -15,6 +39,15 @@ export const AccountShow = (props) => (
           <TextField source="accounttype.name" />
           <NumberField source="xpub.id" />
           <NumberField source="accounttype.id" />
+          <Button
+              className={classes.actions}
+              variant="contained"
+              href="/#/transactions" // TODO: API call to transactions/account/:accountId
+              startIcon={<ReceiptIcon />}
+          >
+              View Transactions
+          </Button>
         </SimpleShowLayout>
     </Show>
-);
+  )
+}
