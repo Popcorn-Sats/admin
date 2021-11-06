f731af654b92e4d2e56c28fbcbd90c6f2cdd0a39
Zane Pocock <zanepocock@gmail.com>
2021-11-06 14:10:17 -0500

    Transaction List View

---
+++ src/transactions.js
@@ -0,0 +1,20 @@
+import * as React from "react"
+import { List, Datagrid, TextField, RichTextField, DateField } from 'react-admin'
+
+export const TransactionList = props => (
+  <List {...props}>
+      <Datagrid rowClick="edit">
+          <DateField label="Date" source="block.date" />
+          <TextField label="Type" source="transactiontype.type" />
+          <TextField label="Category" source="category.name" />
+          <RichTextField source="description" />
+          <TextField source="balance_change" />
+          {/* <TextField source="txid" />
+          <TextField source="address" />
+          <TextField source="network_fee" />
+          <TextField source="size" />
+          <TextField label="Block height" source="block.height" />
+          <ArrayField source="transactionledgers"><SingleFieldList><ChipField source="id" /></SingleFieldList></ArrayField> */}
+      </Datagrid>
+  </List>
+)
