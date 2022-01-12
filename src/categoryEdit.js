26ea05bad10c7280b213421ab7454ad1ee548095
Zane Pocock <zanepocock@gmail.com>
2022-01-11 21:35:41 -0600

    Edit Success Notify

--- src/categoryEdit.js
+++ src/categoryEdit.js
@@ -1,15 +1,27 @@
 import * as React from "react"
-import { Edit, SimpleForm, TextInput } from 'react-admin'
+import { useNotify, useRefresh, useRedirect, Edit, SimpleForm, TextInput } from 'react-admin'
 
 const CategoryTitle = ({ record }) => {
   return <span>Category {record ? `"${record.id}"` : ''}</span>;
 };
 
-export const CategoryEdit = (props) => (
-  <Edit title={<CategoryTitle />} {...props}>
-      <SimpleForm>
-          <TextInput disabled source="id" />
-          <TextInput source="name" />
-      </SimpleForm>
-  </Edit>
-);
+export const CategoryEdit = (props) => {
+  const notify = useNotify();
+  const refresh = useRefresh();
+  const redirect = useRedirect();
+
+  const onSuccess = ({ data }) => {
+    notify(`Changes to category "${data.name}" saved`);
+    redirect('/categories');
+    refresh();
+  };
+
+  return (
+    <Edit onSuccess={onSuccess} mutationMode="pessimistic" title={<CategoryTitle />} {...props}>
+        <SimpleForm>
+            <TextInput disabled source="id" />
+            <TextInput source="name" />
+        </SimpleForm>
+    </Edit>
+  )
+};
