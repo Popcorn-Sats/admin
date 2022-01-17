43a0754a2ef849ce9b8e370ad4faaf01723c39b0
Zane Pocock <zanepocock@gmail.com>
2022-01-16 21:20:35 -0600

    Bootstrap dashboard in tsx

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ import { CategoryList } from "./categories"
 import { CategoryShow } from "./categoryShow"
 import { CategoryEdit } from "./categoryEdit"
 import { CategoryCreate } from "./categoryCreate"
+import { Dashboard } from "./dashboard"
 import { TransactionList } from "./transactions"
 import { TransactionShow } from './transactionShow'
 import { TransactionEdit } from "./transactionEdit"
 
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource name="accounts" list={AccountList} show={AccountShow} edit={AccountEdit} create={AccountCreate} />
     <Resource name="categories" list={CategoryList} show={CategoryShow} edit={CategoryEdit} create={CategoryCreate} />
     <Resource name="transactions" list={TransactionList} show={TransactionShow} edit={TransactionEdit} />
