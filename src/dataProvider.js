27050a2ddb73eaba7012386af4b81f6158a316c7
Zane Pocock <zanepocock@gmail.com>
2022-03-20 14:56:27 -0500

    Fix sort & order on dataProvider

--- src/dataProvider.js
+++ src/dataProvider.js
@@ -10,7 +10,8 @@ const dataProvider = {
         const { page, perPage } = params.pagination;
         const { field, order } = params.sort;
         const query = {
-            sort: JSON.stringify([field, order]),
+            sort: field,
+            order,
             page,
             perPage,
             range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
@@ -53,7 +54,8 @@ const dataProvider = {
         const { page, perPage } = params.pagination;
         const { field, order } = params.sort;
         const query = {
-            sort: JSON.stringify([field, order]),
+            sort: field,
+            order,
             page,
             perPage,
             range: JSON.stringify([(page - 1) * perPage, page * perPage - 1]),
